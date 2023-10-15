bfbcf67faa72a04ce1e46e17b9f7ae3f6375e9e7
Rasim <rasimseferli98@gmail.com>
2023-10-15 15:32:09 +0400

    first commit

---
+++ src/test/java/com/bookdelivery/BookDelivery/BookDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookdelivery.BookDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
